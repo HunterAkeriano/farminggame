1ffb96ed8e728dea6b5398215d34edbf0a400731
HunterAkeriano <stajectgamer@gmail.com>
2022-09-26 11:33:30 +0300

    reload

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-"use strict"
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
-    async function formSend(e){
-        e.preventDefault();
+$(document).ready(function() {
 
-        let eror = formValidate(form);
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-        let formData = new FormData(form);
-
-        if(eror === 0){
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            });
-            if(response.ok){
-                let result = await response.json();
-                form.reset();
-            }
-        }
-
-    }
-
-    function formValidate(form){
-        let eror = 0;
-        let formReq = document.querySelectorAll('._req');
-        for(let index = 0; index < formReq.length; index++){
-            const input = formReq[index];
-            if(input.classList.contains('_emails')){
-                if(emailTest(input)){
-                    formAddEror(input);
-                    eror++;
-                }
-            }
-        }
-    }
-    function emailTest(input){
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?w+)*(\.\w{2,8})+$/.test(input.value);
-    }
 });
